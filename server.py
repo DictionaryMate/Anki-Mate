a030c52175d5551318199c85b5ae53482fd7c517
adaxiang <adaxiang3104@gmail.com>
2024-08-31 14:37:24 +0800

    added public url

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 from textual_serve.server import Server
 
-server = Server("python -m src.main", host="0.0.0.0", port=8080)
+server = Server(
+    "python -m src.main",
+    host="0.0.0.0",
+    port=8080,
+    public_url="https://anki-mate.onrender.com",
+)
 server.serve()
