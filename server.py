f9234fe8360ac38f035baf5380df4b4c72331ab6
adaxiang <adaxiang3104@gmail.com>
2024-09-01 11:32:29 +0800

    remove serve

--- server.py
+++
@@ -1,9 +0,0 @@
-from textual_serve.server import Server
-
-server = Server(
-    "python -m src.main",
-    host="0.0.0.0",
-    port=8080,
-    public_url="https://anki-mate.onrender.com",
-)
-server.serve()
